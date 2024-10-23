7d81f61fef4c838bbdaa9ee88d87b4a6d6b727bc
Gorkem <Gorkem@DESKTOP-2DV200H>
2024-10-23 11:42:57 -0700

    Add project files.

---
+++ Polymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public abstract class BaseGeometrikSekil //Base class
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public BaseGeometrikSekil(double genislik, double yukseklik)//Constructor
+        {
+            this.Width = genislik;
+            this.Height = yukseklik;
+        }
+
+        public abstract double AlanHesapla();
+
+    }
+    public class Kare : BaseGeometrikSekil
+    {
+        public Kare(double kenar) : base(kenar, kenar) { }//Constructor
+        public override double AlanHesapla()
+        {
+            return Width * Height; // Kenar * Kenar
+        }
+
+    }
+    public class Dikdortgen : BaseGeometrikSekil
+    {
+        public Dikdortgen(double genislik, double yukseklik) : base(genislik, yukseklik) { }//Constructor
+        public override double AlanHesapla()
+        {
+           return Height * Width; // Kenar * Kenar
+        }
+    }
+    public class Ucgen : BaseGeometrikSekil
+    {
+        public Ucgen(double genislik, double yukseklik) : base(genislik, yukseklik) { }//Constructor
+        public override double AlanHesapla()
+        {
+            return (Width * Height) / 2;
+        }
+    }
+
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,11 @@
+﻿using Polymorphism;
+
+BaseGeometrikSekil kare = new Kare(8);
+BaseGeometrikSekil dikdortgen = new Dikdortgen(5, 6);
+BaseGeometrikSekil ucgen = new Ucgen(4, 6);
+
+Console.WriteLine($"Karenin Alanı: {kare.AlanHesapla()}");
+Console.WriteLine($"Dikdörtgenin Alanı: {dikdortgen.AlanHesapla()}");
+Console.WriteLine($"Dik Üçgenin Alanı: {ucgen.AlanHesapla()}");
+
+Console.ReadKey();
